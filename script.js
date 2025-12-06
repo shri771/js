56f6ba0b0e7e4fa279c4800a7a470b74f3d4d072
Shrikant Shingare <shrikantshingare77@gmail.com>
2025-12-07 00:04:35 +0530

    added project

---
+++ script.js
@@ -0,0 +1,59 @@
+// Elements
+const typeArea = document.querySelector(".textarea textarea");
+const quoteArea = document.querySelector(".quotes-area");
+
+// Buttons
+const saveBtn = document.querySelector(".save-btn");
+
+// Variables
+const savedQuotes = localStorage.getItem("savedQuotes");
+let quoteArr = savedQuotes ? JSON.parse(savedQuotes) : [];
+
+// start
+function init() {
+  quoteArr.forEach(function (quote) {
+    generateQuote(quote.quote, quote.quoteId);
+  });
+}
+init();
+
+// Event Listener
+saveBtn.addEventListener("click", function () {
+  const quote = typeArea.value;
+  if (!quote) return;
+  const quoteId = shortid();
+  generateQuote(quote, quoteId);
+
+  quoteArr.push({
+    quote: quote,
+    quoteId: quoteId,
+  });
+  updateLS();
+  typeArea.value = "";
+});
+
+quoteArea.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete-btn")) {
+    const quoteId = e.target.id;
+    quoteArr = quoteArr.filter((quote) => quote.quoteId !== quoteId);
+    e.target.closest(".quote-card").remove();
+    updateLS();
+  }
+});
+
+function generateQuote(quote, quoteID) {
+  const quoteCard = document.createElement("div");
+  quoteCard.setAttribute("class", "quote-card");
+  quoteCard.innerHTML = `
+          <div class="quote-id" >${quoteID}</div>
+          <div class="quote-text">
+          ${quote}
+          </div>
+          <button class="delete-btn" id="${quoteID}">🗑️</button>
+`;
+  quoteArea.appendChild(quoteCard);
+}
+
+function updateLS() {
+  localStorage.setItem("savedQuotes", JSON.stringify(quoteArr));
+}
